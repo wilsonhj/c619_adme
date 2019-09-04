9295171187b68cd2730f1e47b6481f7bde631ef2
Samantha Durant <samdurant.teacher@gmail.com>
2019-09-04 22:10:37 +0000

    define set view method on App component and bind it

--- client/app.jsx
+++ client/app.jsx
@@ -4,8 +4,21 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: null
+      view: {
+        name: 'landing-page',
+        params: {}
+      }
     };
+    this.setView = this.setView.bind(this);
+  }
+
+  setView(name, params) {
+    this.setState({
+      view: {
+        name: name,
+        params: params
+      }
+    });
   }
 
   render() {
