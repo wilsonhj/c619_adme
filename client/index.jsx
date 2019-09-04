808cd4d0ae1caf51b3d874719942b9592ba4fcdd
smcgrath0 <smcgrath7777@gmail.com>
2019-09-04 12:24:57 -0700

    setup webpack set up react hello world

---
+++ client/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>hi</h1>,
+  document.querySelector('#root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const srcPath = path.resolve(__dirname, 'client');
+const publicPath = path.resolve(__dirname, 'server/public');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  entry: './client',
+  output: {
+    path: publicPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: srcPath,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-transform-react-jsx'
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
