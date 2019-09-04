0ed9faecf445780164c739cf652fe5d3026cabc2
smcgrath0 <smcgrath7777@gmail.com>
2019-09-04 12:46:16 -0700

    front end and backend connected if reached at localhost:3000/api

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const companies = require('./companies');
 const creators = require('./creators');
 const server = express();
 let PORT = process.env.PORT;
-server.use('/companies', companies);
-server.use('/creators', creators);
+server.use('/api/companies', companies);
+server.use('/api/creators', creators);
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('its listening');
