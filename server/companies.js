ddf6f7e24fc0360cf987e8550c4e975f825b4145
smcgrath0 <smcgrath7777@gmail.com>
2019-09-04 17:47:26 -0700

    Create dummy data for example response body Make endpoint for company in seperate file using routers

--- server/companies.js
+++ server/companies.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/:id', (req, res, next) => {
-  // var companyID = req.params.id;
+  var companyID = req.params.id;
 
-  res.json({ companyname: 'Target', companyLogo: '', companyID: 1 });
+  res.json({ companyname: 'Target', companyLogo: 'https://www.aiga.org/globalassets/aiga/content/inspiration/corporate-awards/2006-target/target_header.png', companyID });
 });
 
 module.exports = router;
