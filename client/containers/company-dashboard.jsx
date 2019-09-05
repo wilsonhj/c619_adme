6fc6700360724efa88a0f167cbde5ba87841e9d9
smcgrath0 <smcgrath7777@gmail.com>
2019-09-05 15:49:14 -0700

    added fetch

--- client/containers/company-dashboard.jsx
+++ client/containers/company-dashboard.jsx
@@ -3,7 +3,14 @@ import Page from '../components/page';
 import AppContext from '../context';
 
 export default class CompanyDashboard extends React.Component {
-
+  componentDidMount() {
+    fetch('api/companies/1')
+      .then(res => res.json())
+      .then(response => {
+        // eslint-disable-next-line no-console
+        console.log(response);
+      });
+  }
   render() {
     const setView = () => this.context.setView('landing-page', {});
     return (
