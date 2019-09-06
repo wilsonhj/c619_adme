3f693b66eb28f88a0eee6712102a801286480d13
ssairafe <sariaphd@gmail.com>
2019-09-05 18:06:09 -0700

    index renders header

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import NavBar from './components/nav-bar';
 
 ReactDOM.render(
-  <App />,
+  <>
+  <NavBar/>,
+  <App />
+  </>,
   document.querySelector('#root')
 );
