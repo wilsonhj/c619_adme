2547c629d98fd17a5aa279ee47fddab905f96788
smcgrath0 <smcgrath7777@gmail.com>
2019-09-06 09:35:28 -0700

    add and connect it in the index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const connection = require('./connection');
 const express = require('express');
 const companies = require('./companies');
 const creators = require('./creators');
+const campaigns = require('./campaigns');
 
 const server = express();
 let PORT = process.env.PORT;
@@ -11,6 +12,7 @@ connection.connect();
 
 server.use('/api/companies', companies);
 server.use('/api/creators', creators);
+server.use('/api/campaigns', campaigns);
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('its listening');
