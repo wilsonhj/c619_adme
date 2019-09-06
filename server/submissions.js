b9e88c9f8dcaeac14a7291104f5a5daed31d70f9
johnwsouth <im.john.south@gmail.com>
2019-09-06 20:23:28 +0000

    created multer storage path in submissions.js

--- server/submissions.js
+++ server/submissions.js
@@ -2,14 +2,29 @@ const express = require('express');
 // const connection = require('./connection');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const path = require('path');
 
-router.post('/', upload.single('submissionImage'), (req, res, next) => {
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, 'public/uploads'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/', upload.single('submissionContent'), (req, res, next) => {
 
   // connection.query('SELECT * FROM `creators` WHERE `creatorID` = `' + creatorID + '`', (err, rows, fields) => {
   // if (err) throw err;
+  let response = {
+    requestBody: req.body,
+    requestFileStorageInfo: req.file
+  };
+  res.json(response);
 
-  res.json(req);
   // });
 
 });
