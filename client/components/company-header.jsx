a40dc49413d9888f5a9f3358dbf380e8e6abbfbb
James Yamagata <wilsonhj@uci.edu>
2019-09-06 16:26:12 -0700

    finished Company Details Header revised

--- client/components/company-header.jsx
+++ client/components/company-header.jsx
@@ -4,10 +4,10 @@ import { Card, CardImg, Button } from 'reactstrap';
 export default function CompanyHeader(props) {
   return (
     <div>
-      <Card className="text-center light" style={{ backgroundColor: '#242038' }}>
-        <h3 style={{ color: 'white' }} className="shadow">{props.companyName}</h3>
-        <CardImg src={'https://files.slack.com/files-pri/T1EHQUJ8J-FN3SSC17S/targetlogo.png'} className="rounded mx-auto shadow" style={{ width: '150px', height: '150px', backgroundSize:'contain' }}></CardImg>
-        <Button color="light" style={{ width: '250px' }} className="btn mx-auto mt-3 mb-3">Create Campaign</Button>{' '}
+      <Card className="text-center" style={{ backgroundColor: '#242038' }}>
+        <h3 style={{ color: 'white' }}>{props.companyName}</h3>
+        <CardImg src={props.companyLogo} className="rounded mx-auto" style={{ height: '100px', width: '100px', backgroundSize: 'cover' }}></CardImg>
+        <Button color="light" style={{ width: '200px' }} className="btn mx-auto mt-3 mb-3">Create Campaign</Button>{' '}
       </Card>
     </div>
   );
