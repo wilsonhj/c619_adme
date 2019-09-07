b6d2c7ba767b444cdae6c1a55c4fa98a8ffc5208
smcgrath0 <smcgrath7777@gmail.com>
2019-09-07 14:48:43 -0700

    fixed broken, trash design

--- client/components/company-header.jsx
+++ client/components/company-header.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Card, CardImg, Button } from 'reactstrap';
+// import { Card, CardImg, Button } from 'reactstrap';
 
 export default function CompanyHeader(props) {
   return (
-    <div>
-      <Card className="text-center" style={{ backgroundColor: '#242038' }}>
+    <div className="row background-light justify-content-around align-items-center companyHeader" style={{ backgroundColor: '#CAC4CE' }}>
+      {/* <Card className="text-center" style={{ backgroundColor: '#242038' }}>
         <h3 style={{ color: 'white' }}>{props.companyName}</h3>
         <CardImg src={props.companyLogo} className="rounded mx-auto" style={{ height: '100px', width: '100px', backgroundSize: 'cover' }}></CardImg>
         <Button color="light" style={{ width: '200px' }} className="btn mx-auto mt-3 mb-3">Create Campaign</Button>{' '}
-      </Card>
+      </Card> */}
+      {/* <div className="ml-5"> */}
+      <img src={props.companyLogo} className="rounded-circle" style={{ backgroundSize: 'contain', height: '75px' }}></img>
+      <div className="mt-1" style={{ color: '#9067C6' }}>
+        <p className="mb-0">{props.companyName}</p>
+        <p className="ml-1 mb-1" style={{ fontSize: '0.8rem' }}>{props.companyType}</p>
+        <button className="btn my-auto createCampaignButton" style={{ height: '30px', width: '130px', backgroundColor: 'white', fontSize: '0.75rem' }}>Create Campaign</button>
+      </div>
+
+      {/* </div> */}
+
     </div>
   );
 }
--- client/containers/company-dashboard.jsx
+++ client/containers/company-dashboard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Page from '../components/page';
 import AppContext from '../context';
 import CompanyHeader from '../components/company-header.jsx';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
 
 export default class CompanyDashboard extends React.Component {
 
@@ -11,8 +13,11 @@ export default class CompanyDashboard extends React.Component {
     this.state = {
       companyID: '',
       companyName: '',
-      companyLogo: ''
+      companyLogo: '',
+      companyType: '',
+      activeTab: '1'
     };
+    this.toggle = this.toggle.bind(this);
   }
   componentDidMount() {
     this.retrieveCompanyData(1);
@@ -20,16 +25,55 @@ export default class CompanyDashboard extends React.Component {
   retrieveCompanyData(id) {
     fetch(`/api/companies/${id}`)
       .then(res => res.json())
-      .then(res => this.setState({ companyID: res.companyID, companyName: res.companyName, companyLogo: res.companyLogo }));
+      .then(res => this.setState({ companyID: res.companyID, companyName: res.companyName, companyLogo: res.companyLogo, companyType: res.companyType }));
+  }
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
   }
   render() {
     const setView = () => this.context.setView('landing-page', {});
     const goToCreateCampaign = () => this.context.setView('create-campaign', {});
     return (
-      <>
+      <div style={{ width: '100%' }}>
         <Page title='Company Dashboard' setView={setView} />
-        <CompanyHeader companyID={this.state.companyID} companyName={this.state.companyName} companyLogo={this.state.companyLogo} setView={goToCreateCampaign}></CompanyHeader>
-      </>
+        <CompanyHeader companyID={this.state.companyID} companyName={this.state.companyName} companyLogo={this.state.companyLogo} companyType={this.state.companyType} setView={goToCreateCampaign}></CompanyHeader>
+        <div className="col light tab-text p-0" style={{ backgroundColor: '#CAC4CE' }}>
+          <Nav tab>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: this.state.activeTab === '1' })}
+                onClick={() => { this.toggle('1'); }}
+              >
+                    Current Campaigns
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: this.state.activeTab === '2' })}
+                onClick={() => { this.toggle('2'); }}
+              >
+                    Previous Campaigns
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={this.state.activeTab}>
+            <TabPane tabId="1">
+              <Row>
+                <Col sm="12 p-0" style={{ width: '100%' }}>
+                  <div className="bg-white p-1 vh-100 creatorTab" >Video Submissions</div>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="2">
+              <div className="bg-white p-1 vh-100 creatorTab" style={{ width: '100%' }}>{this.state.bio}</div>
+            </TabPane>
+          </TabContent>
+        </div>
+      </div>
     );
   }
 
