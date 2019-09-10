970cb253c80c37a2aa60b593f9f89a92b4d95e7d
James Yamagata <wilsonhj@uci.edu>
2019-09-09 17:14:30 -0700

    finished create campaign mobile FE

--- client/components/company-header.jsx
+++ client/components/company-header.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
-// import { Card, CardImg, Button } from 'reactstrap';
 
 export default function CompanyHeader(props) {
+  const style = {};
+  style.image = {
+    backgroundSize: 'contain',
+    height: '75px'
+  };
+  style.div = {
+    color: '#9067C6'
+  };
+  style.button = {
+    height: '30px',
+    width: '130px',
+    backgroundColor: 'white',
+    fontSize: '0.75rem'
+  };
+
   return (
     <div className="row background-light justify-content-around align-items-center companyHeader" style={{ backgroundColor: '#CAC4CE' }}>
-      {/* <Card className="text-center" style={{ backgroundColor: '#242038' }}>
-        <h3 style={{ color: 'white' }}>{props.companyName}</h3>
-        <CardImg src={props.companyLogo} className="rounded mx-auto" style={{ height: '100px', width: '100px', backgroundSize: 'cover' }}></CardImg>
-        <Button color="light" style={{ width: '200px' }} className="btn mx-auto mt-3 mb-3">Create Campaign</Button>{' '}
-      </Card> */}
-      {/* <div className="ml-5"> */}
-      <img src={props.companyLogo} className="rounded-circle mt-1" style={{ backgroundSize: 'contain', height: '75px' }}></img>
-      <div className="mt-1" style={{ color: '#9067C6' }}>
+      <img src={props.companyLogo} className="rounded-circle mt-1" style={style.image}></img>
+      <div className="mt-1" style={style.div}>
         <p className="mb-0">{props.companyName}</p>
         <p className="ml-1 mb-1" style={{ fontSize: '0.8rem' }}>{props.companyType}</p>
-        <button className="btn my-auto createCampaignButton" onClick={() => props.setView()}style={{ height: '30px', width: '130px', backgroundColor: 'white', fontSize: '0.75rem' }}>Create Campaign</button>
+        <button className="btn my-auto createCampaignButton" onClick={() => props.setView()} style={style.button}>Create Campaign</button>
       </div>
-
-      {/* </div> */}
-
     </div>
   );
 }
--- client/containers/create-campaign.jsx
+++ client/containers/create-campaign.jsx
@@ -1,14 +1,71 @@
 import React from 'react';
+import AppContext from '../context';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 export default class CreateCampaign extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: '',
+      requirements: '',
+      runSpace: '',
+      rewards: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState(
+      { [event.target.name]: event.target.value }
+    );
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    var data = new FormData(event.target);
+    data.append('submissionsReceived', '0');
+    data.append('companyID', '1');
+    fetch('http://localhost:3000/api/campaigns',
+      {
+        method: 'POST',
+        body: data
+      })
+      .then(res => res.json())
+      .then(this.context.setView('company-dashboard', {}));
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          <input />
-          <input type='file'/>
-        </form>
-      </div>
+      <Form className="p-4 rounded m-3 creatorSubmissionForm" onSubmit={this.handleSubmit}>
+        <h4 className="mb-3 mx-auto font-weight-bold">Upload Your Submission</h4>
+        <FormGroup>
+          <Label>Description </Label>
+          <Input className="shadow-sm" type="text" name="description" onChange={this.handleChange} required/>
+        </FormGroup>
+        <FormGroup>
+          <Label>Requirements </Label>
+          <Input className="shadow-sm" type="text" name="requirements" onChange={this.handleChange} required/>
+        </FormGroup>
+        <FormGroup>
+          <Label>Run Space </Label>
+          <Input className="shadow-sm" type="text" name="runSpace" onChange={this.handleChange} required/>
+        </FormGroup>
+        <FormGroup>
+          <Label>Rewards</Label>
+          <Input className="shadow-sm" onChange={this.handleChange} type="text" name="rewards" required/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="file">File</Label>
+          <Input onChange={this.handleChange} type="file" name="campaignContent" id="file" required/>
+          <FormText color="muted">
+            Please upload a file:
+          </FormText>
+        </FormGroup>
+        <Button className="shadow creatorSubmitButton">Submit</Button>
+      </Form>
     );
   }
 }
+
+CreateCampaign.contextType = AppContext;
