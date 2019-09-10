d51d58866095688a9aaf0964ebdd3138738f5723
johnwsouth <im.john.south@gmail.com>
2019-09-10 07:47:33 +0000

    added like button feature to backend at endpoint /apis/submissions/likes

--- server/submissions.js
+++ server/submissions.js
@@ -15,6 +15,21 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+router.get('/', (req, res, next) => {
+  connection.query('SELECT * FROM `submissions`', (err, rows, fields) => {
+    if (err) throw err;
+    res.send(rows);
+  });
+});
+
+router.put('/likes', upload.single(''), (req, res, next) => {
+  let params = [req.body.likes, req.body.submissionID];
+  connection.execute('UPDATE submissions SET `likes` = ? WHERE `submissionID` = ?', params, (err, rows, fields) => {
+    if (err) throw err;
+    res.send(rows);
+  });
+});
+
 router.post('/', upload.single('submissionContent'), (req, res, next) => {
   let params = [req.body.creatorID, req.body.typeOfContent, req.file.path, req.body.title, req.body.requestID, req.body.likes];
 
