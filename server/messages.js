44f58d0f3057b8f154d995481715e8c91c97dfb9
smcgrath0 <smcgrath7777@gmail.com>
2019-09-12 13:01:52 -0700

    added basic message setuip

---
+++ server/messages.js
@@ -0,0 +1,16 @@
+// // const express = require('express');
+// // const connection = require('./connection');
+// const io = require('socket.io')(3002);
+// // const router = express.Router();
+
+// const users = {};
+
+// io.on('connection'), socket => {
+//   socket.emit('chat-message', 'message');
+
+//   socket.on('send-chat-message', message => {
+//     socket.broadcast.emit('chat-message', message);
+//   });
+// };
+
+// module.exports = router;
