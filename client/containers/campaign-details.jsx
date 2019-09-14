9ed4bfc9df2df7fad8f1c847ef5e007241bda5cf
wilsonhj <wilsonhj@uci.edu>
2019-09-14 00:20:35 +0000

    almost done with campaign details page

--- client/containers/campaign-details.jsx
+++ client/containers/campaign-details.jsx
@@ -67,19 +67,29 @@ export default class ViewCampaignDetails extends React.Component {
     const submissions = this.state.submissions.map(submissionObj => {
       return (
         <React.Fragment key={submissionObj.submissionID}>
-          <h1>{submissionObj.submissionTitle}</h1>
+          <h4 className="mt-5 text-center">{submissionObj.submissionTitle}</h4>
           <video src={submissionObj.submissionContent} poster={submissionObj.submissionThumbnail}
             className="mx-auto my-2 shadow" style={{ width: '95%' }} controls>
           </video>
-          <div>{submissionObj.submissionDescription}</div>
-          <div>{submissionObj.likes}</div>
+          <p>{submissionObj.submissionDescription}</p>
+          <div className="p-3 fas fa-heart text-right" style={{ color: 'rgb(132, 29, 158)' }}>
+            {submissionObj.likes}
+          </div>
         </React.Fragment>
       );
     });
     return (
       <div className="creatorInfoContainer shadow rounded d-flex flex-column justify-content-center m-2 pb-4 pt-2" >
-        <h4 className="text-center mt-2">{this.state.campaignDetails.campaignTitle}</h4>
-        <p>{this.state.campaignDetails.description}</p>
+        {this.context.currentUser.creatorID
+          ? <div className="d-inline ml-2 fas fa-arrow-left" onClick = {() => {
+            this.context.setView('creator-portfolio', {});
+          }} style={{ width: '10%', fontSize: '7.5vmin', color: 'rgba(132, 29, 158, .8)' }}></div>
+          : <div className="d-inline ml-2 fas fa-arrow-left" onClick={() => {
+            this.context.setView('company-dashboard', {});
+          }} style={{ width: '10%', fontSize: '7.5vmin', color: 'rgba(132, 29, 158, .8)' }}></div>
+        }
+        <h1 className="text-center mt-2 submission-details-title">{this.state.campaignDetails.campaignTitle}</h1>
+        <p className="text-center mt-2 ">{this.state.campaignDetails.description}</p>
         {submissions}
       </div>
     );
