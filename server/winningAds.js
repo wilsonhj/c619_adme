c3f5fe4bf654fb891c15d628b359501632ff67f2
smcgrath0 <smcgrath7777@gmail.com>
2019-09-16 10:44:17 -0700

    added winning ads enpoints and stars campaign redesigned

--- server/winningAds.js
+++ server/winningAds.js
@@ -3,10 +3,12 @@ const connection = require('./connection');
 const router = express.Router();
 
 // pick a winner by submissionID
-router.post('/:id', (req, res, next) => {
+router.post('/:submisionID', (req, res, next) => {
   let params = [
-    req.params.id
+    req.params.submissionID
+    // req.body.campaignID
   ];
+  // connection.execute(`SELECT * FROM winningAds`)
   connection.execute(`INSERT INTO winningAds (submissionID) VALUES ( ? );`, params, (err, rows, fields) => {
     if (err) throw err;
     res.json(rows);
