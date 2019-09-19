1bb621cd5ee4e0d92a6af81324d284d588bd35b4
Samantha Durant <samdurant.teacher@gmail.com>
2019-09-19 20:37:23 +0000

    completed some changes to styling. made creator tabs cleaner and images centered on landing page

--- client/containers/landing-page.jsx
+++ client/containers/landing-page.jsx
@@ -54,7 +54,7 @@ export default class LandingPage extends React.Component {
       return (
         <div className='winningCard' style={{ display: 'inline-block', width: '25em', height: '20em', margin: '1rem', borderRadius: '10%' }} key={ads.submissionID} >
 
-          <div className='profilePicSmall' onClick={() => { this.context.setView('creator-portfolio', { creatorID: ads.creatorID }); }} style={{ marginLeft: '8%', marginTop: '7%', border: 'solid .12em yellow', height: '5em', width: '5em', borderRadius: '10%', backgroundImage: 'url(' + ads.profilePicture + ')', backgroundSize: 'contain', display: 'inline-block', backgroundRepeat: 'no-repeat' }} key={ads.creatorID}>
+          <div className='profilePicSmall' onClick={() => { this.context.setView('creator-portfolio', { creatorID: ads.creatorID }); }} style={{ marginLeft: '8%', marginTop: '7%', border: 'solid .12em yellow', height: '5em', width: '5em', borderRadius: '10%', backgroundImage: 'url(' + ads.profilePicture + ')', backgroundSize: 'cover', backgroundPosition: 'center', display: 'inline-block', backgroundRepeat: 'no-repeat' }} key={ads.creatorID}>
           </div>
           <div className="row" style={{ marginTop: '10%', verticalAlign: 'top', display: 'inline-block' }}>
             <div style={{ marginLeft: '20%', display: 'inline-block' }}>
@@ -75,7 +75,7 @@ export default class LandingPage extends React.Component {
       return (
         <div className='glassCard' style={{ display: 'inline-block', width: '25em', height: '20em', margin: '1rem', borderRadius: '10%' }} key={ads.submissionID} >
 
-          <div className='profilePicSmall' onClick={() => { this.context.setView('creator-portfolio', { creatorID: ads.creatorID }); }} style={{ marginLeft: '8%', marginTop: '7%', border: 'solid .12em #841D9E', height: '5em', width: '5em', borderRadius: '10%', backgroundImage: 'url(' + ads.profilePicture + ')', backgroundSize: 'contain', display: 'inline-block', backgroundRepeat: 'no-repeat' }} key={ads.creatorID}>
+          <div className='profilePicSmall' onClick={() => { this.context.setView('creator-portfolio', { creatorID: ads.creatorID }); }} style={{ marginLeft: '8%', marginTop: '7%', border: 'solid .12em #841D9E', height: '5em', width: '5em', borderRadius: '10%', backgroundImage: 'url(' + ads.profilePicture + ')', backgroundSize: 'cover', backgroundPosition: 'center', display: 'inline-block' }} key={ads.creatorID}>
           </div>
           <div className="row" style={{ marginTop: '10%', verticalAlign: 'top', display: 'inline-block' }}>
             <div style={{ marginLeft: '20%', display: 'inline-block' }}>
